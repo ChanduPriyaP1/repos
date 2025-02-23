f4fe23fadd80a2774e4ff82595f93a739be0c391
Siva <sivakumarpulis@gmail.com>
2025-02-23 17:14:18 +0530

    commited

--- Shell-Script/read.sh
+++ Shell-Script/read.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 
-echo " Enter UserName : $Username" # We call Variables using '$' symbol.
+echo " Enter UserName :" # We call Variables using '$' symbol.
 read Username # Here UserName is Variable
 
-echo " Enter Passward : $Passward"
+echo " Enter Passward : " # Here we can Saw Passward. I wanna hide Passward.
 read Passward # Here Passward is Variable
+
+echo " Enter UserName1 :" # Now hide the Username
+read  -s Username1 
+
+echo " Enter Passward1 : " # Now hide the Passward
+read -s Passward1 
+
+echo "UserName : $Username1"
+echo "Passward : $Passward1" 
