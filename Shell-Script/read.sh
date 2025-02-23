1e15d8e489a181993a57823c42d2ee056cf888d6
Siva <sivakumarpulis@gmail.com>
2025-02-23 17:07:05 +0530

    commited

---
+++ Shell-Script/read.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read Username # Here UserName is Variable
+echo " Enter UserName : $Username" # We call Variables using '$' symbol.
+read Passward # Here Passward is Variable
+echo " Enter Passward : $Passward"
