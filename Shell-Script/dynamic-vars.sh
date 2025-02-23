608819a619334dbf223d49d508fe4cf1c3287615
Siva <sivakumarpulis@gmail.com>
2025-02-23 16:15:44 +0530

    commited

--- Shell-Script/dynamic-vars.sh
+++ Shell-Script/dynamic-vars.sh
@@ -1,5 +1,5 @@
 Persion=$1
 a=$2
 b=$3
-echo "i am $1. I wanna buy somthing upto $($2+$3) rupees only"
+echo "i am $1. I wanna buy somthing upto $(($2+$3)) rupees only"
 # ./dynamin-vars.sh sindu 5 7
