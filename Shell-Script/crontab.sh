4591401b38bfcf7debd481e5f7e9b64782de73ba
Siva <sivakumarpulis@gmail.com>
2025-03-06 16:55:40 +0530

    commited

--- Shell-Script/crontab.sh
+++ Shell-Script/crontab.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-# Deleting old logs
-source ./1-colors.sh
-
 SOURCE_DIRECTORY=/tmp/app-log
 
-if[ -d $SOURCE_DIRECTORY ]
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
 then
-    echo -e "$m Source Directory is Exists$N"
+    echo -e "$G Source directory exists $N"
 else
-    echo -e "$G please make sure $SOURCE_DIRECTORY exist$N"  
-    exit 1  
- fi   
- FILE=(find $SOURCE_DIRECTORY -name "*.log" -mtime +30) 
- while IFS=read -r line
- do 
-    echo -e "$Y Deliting Files: $line $N"
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
+fi
+
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+while IFS= read -r line
+do
+    echo "Deleting file: $line"
     rm -rf $line
- done <<<FILE
- 
+done <<< $FILES
